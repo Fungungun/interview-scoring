e8a8f5c8ff349628b2ce013b3582a9bd02e7a97c
cliu <liu_chenghuan@126.com>
2021-01-16 21:34:25 +0800

    fix db bug

--- scoring/scoring/settings.py
+++ scoring/scoring/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'scoring.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.bug.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
